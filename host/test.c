0c50a1d137b1fdb61cc8fe6a10c5819ff130814c
Champagne Lewis <champagne.t.lewis@gmail.com>
2018-11-18 03:01:19 -0500

    In the midst of testing, breaking, fixing...
    For a bit I had reading and writing through mdblib working,
    but somehow I broke it while trying to get more than writing and
    reading single bytes working. I couldn't even pinpoint what went,
    as both reading and writing appear to work, but the
    `VOICE_OF_GOD_OUT` value never appears to change from `0x00`. One
    issue that's been evident since before I got mdblib working was
    that a write watchpoint on `VOICE_OF_GOD_OUT` never works, so I
    had been just waiting for it to update by calling `mdb_continue()`
    repeatedly, and that had been working. No ammount of calling that
    appears to work now, so something else has gone wrong...

--- host/test.c
+++ host/test.c
@@ -1,10 +1,21 @@
+#include <errno.h>
+#include <inttypes.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "voglib_host.h"
 
 
+#define GETC(stream)	({char __clear_input_char = getc(stream);	\
+						__clear_input_char == '\n'					\
+							? getc(stream)							\
+							: __clear_input_char;					\
+						})
+
+
 #ifndef VOG_CLIENT_TEST
-#define VOG_CLIENT_TEST "/home/wizerdwolf/Projects/voglib/client/dist/default/production/client.production.hex"
+#define VOG_CLIENT_TEST "/home/wizerdwolf/Projects/voglib/client/dist/default/production/client.production.elf"
 #endif
 
 #ifndef VOG_CLIENT_DEV
@@ -14,23 +25,55 @@
 
 int main()
 {
+	printf("Initializing pic...\n");
 	vogdev *pic = vog_init(VOG_CLIENT_DEV, VOG_CLIENT_TEST);
+	printf("Pic initialized\n");
 	vog_run(pic);
-
+/*
 	int loop = 1;
-	uint8_t data = 0;
+	size_t basesize = 100;
+	size_t size;
+	size_t bread;
+	uint8_t byte = 0;
+	uint8_t *data = NULL;
 	while (loop) {
-		printf("INPUT:\t");
-		data = getchar();
+		bread = 0;
+		size = 0;
 
-		if (data == 'q')
+		printf("INPUT:\t");
+		byte = getchar();
+		while (byte != '\n') {
+			if (bread >= size) {
+				size += basesize;
+				data = realloc(data, size);
+			}
+
+			data[bread] = byte;
+			byte = getchar();
+			bread++;
+		}
+		data[bread] = '\0';
+		bread++;
+
+		if (strncmp((const char *)data, "quit", strlen("quit")) == 0)
 			loop = 0;
 		else {
-			vog_write_byte(pic, data);
-			vog_read_byte(pic, &data);
-			printf("OUTPUT:\t%c\n", data);
+printf("Data read:\t\"%s\"\n", data);
+			vog_write(pic, data, bread);
+			memset(data, 0, size);
+			vog_read(pic, data, bread);
+			printf("OUTPUT:\t%s\n", data);
 		}
+
+		free(data);
+		data = NULL;
 	}
+*/
+	uint8_t data = 'k';
+	vog_write_byte(pic, data);
+	printf("Wrote:\t%c\t", data);
+	vog_read_byte(pic, &data);
+	printf("Read:\t%c\n", data);
 
 	vog_close(pic);
 }
--- host/voglib_host.c
+++ host/voglib_host.c
@@ -1,6 +1,18 @@
 #include <stdlib.h>
+#include <stdio.h>	// REMOVE_ME
+#include <inttypes.h>	// REMOVE_ME
 
 #include "voglib_host.h"
+
+#ifdef DEBUG
+#define VOG_DBG(format, ...)			\
+	do {									\
+			fprintf(stderr, "%s: %s(): %d:\n\t"format, __FILE__, __func__, __LINE__, ## __VA_ARGS__ );	\
+	} while (0);
+#else
+#define VOG_DBG(format, args...)
+#endif // DEBUG
+
 
 #define MDBLIB	// REMOVE_ME
 
@@ -28,14 +40,24 @@ vogdev *vog_init(char *devname, char *program)
 	vogdev *device = malloc(sizeof(vogdev));
 
 	#ifdef MDBLIB
+	printf("\tUsing mdblib; trying to init\n");	// REMOVE_ME
 	device->interface = mdb_init();
+	printf("\tMdblib initialized:\t0x%"PRIXPTR"\n", (uintptr_t)device->interface);	// REMOVE_ME
 	mdb_device(device->interface, devname);
+	printf("\tDevice set\n");	// REMOVE_ME
 	mdb_hwtool(device->interface, "SIM", 0, 0);
+	printf("\tHwtool set\n");	// REMOVE_ME
 	mdb_program(device->interface, program);
+	printf("\tProgrammed file to device\n");	// REMOVE_ME
 	device->in_addr = mdb_print_var_addr(device->interface, in_name);
+	printf("\n");	// REMOVE_ME
 	device->out_addr = mdb_print_var_addr(device->interface, out_name);
-    mdb_watch(device->interface, device->in_addr, "R", 0);
-    mdb_watch(device->interface, device->out_addr, "W", 0);
+	printf("Reading addresses:\tin_addr: 0x%"MDB_PRIXPTR"\tout_addr: 0x%"MDB_PRIXPTR"\n", device->in_addr, device->out_addr);	// REMOVE_ME
+    int bn1 = mdb_watch_name(device->interface, in_name, "R", 0);
+    printf("1st watchpoint set:\t%d\n", bn1);
+    int bn2 = mdb_watch_name(device->interface, out_name, "RW", 0);
+    //int bn2 = mdb_break_addr(device->interface, device->out_addr, 0);
+    printf("2nd watchpoint set:\t%d\n", bn2);
 
 	#else
 	free(device);
@@ -69,18 +91,22 @@ void vog_run(vogdev *device)
 void vog_read(vogdev *device, uint8_t *buf, size_t count)
 {
 	size_t i;
-	for(i = 0; i < count; i++)
-		vog_read_byte(device, buf[i]);
+	for(i = 0; i < count; i++) {
+		vog_read_byte(device, buf+i);
+printf("Value check:\t\"%s\"\n", buf);
+	}
 }
 
 void vog_read_byte(vogdev *device, uint8_t *data)
 {
 	#ifdef MDBLIB
-	mdb_wait(device->interface);
-	mdb_stepi(device->interface);	// allow the value to be written
-	mdbword output = (mdbword)mdb_print_var(device->interface, 'd', 1, out_name);
+	//mdb_wait(device->interface);
+	while (mdb_state(device->interface) == mdb_running) mdb_noop(device->interface);
+	mdb_step(device->interface);	// allow the value to be written
+	mdbword output = (mdbword)mdb_print_var(device->interface, 'x', 1, out_name);
 	*data = (uint8_t)(0x00FF & output);
 	mdb_continue(device->interface);
+	VOG_DBG("Read 0x%"MDB_PRIWORD" from device\n", output);
 
 	#endif // MDBLIB
 	return;
@@ -97,11 +123,22 @@ void vog_write_byte(vogdev *device, uint8_t data)
 {
 	#ifdef MDBLIB
 	mdbword temp[1];	// mdb_write_mem() requires an array as input
-	temp[0] = (mdbword)mdb_print_var(device->interface, 'd', 1, out_name);
+	temp[0] = (mdbword)mdb_print_var(device->interface, 'x', 1, out_name);
 	temp[0] &= ~0x00FF;	// not 100% the correct endian-ness; will determine with testing
 	temp[0] |= (0x00FF & (mdbword)data);	// back up the word at the address before writing
-	mdb_write_mem(device->interface, 'r', device->out_addr, 1, temp);
+	mdb_write_mem(device->interface, 'r', device->in_addr, 1, temp);
+
+	uint8_t result = (uint8_t)mdb_print_var(device->interface, 'x', 1, in_name);
+	if (data != result) {
+		VOG_DBG("Value failed to write! wrote: 0x%"MDB_PRIWORD" Read: 0x%"MDB_PRIWORD"\n", data, result);
+		exit(1);
+	}
+
+	while (mdb_state(device->interface) == mdb_running) mdb_noop(device->interface);
 	mdb_continue(device->interface);
+	while (mdb_state(device->interface) == mdb_running) mdb_noop(device->interface);
+	mdb_continue(device->interface);
+	VOG_DBG("Wrote 0x%"MDB_PRIWORD" to device\n", temp[0]);
 
 	#endif // MDBLIB
 	return;
